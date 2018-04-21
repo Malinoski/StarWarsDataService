3820315d39a28a868f7c35db37e0b4e3e0ca7f0d
iuri <iuri@10.0.1.109>
2018-04-21 08:09:22 -0300

    Update Readme Bug fix (search wsapi exception)

--- README.md
+++ README.md
@@ -18,13 +18,22 @@
 
 `sudo apt-get install -y tomcat7`
 
-- Clone the project
-
-`git clone https://github.com/Malinoski/StarWarsDataService.git`
-
 - Install MongoDB database
 
 `sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5`
 
 `echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list`
 
+`sudo apt-get update`
+
+`sudo apt-get install -y mongodb-org`
+
+`sudo service mongod start`
+
+- Get and config the project
+
+`git clone https://github.com/Malinoski/StarWarsDataService.git`
+
+`sudo cp StarWarsDataService/StarWarsDataService.war /var/lib/tomcat7/webapps/`
+
+`sudo service tomcat7 restart`
--- src/lib/SwapiHelper.java
+++ src/lib/SwapiHelper.java
@@ -8,34 +8,42 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class SwapiHelper {
-	
+
 	Client c;
+
 	public SwapiHelper() {
-		if(c==null) {
+		if (c == null) {
 			c = Client.create();
 		}
 	}
-	
+
 	public int getFilmsCountFromPlanet(String name) {
 
-		WebResource wr = c.resource("https://swapi.co/api/planets/?search="+name);
-		ClientResponse resp = wr.accept("application/json").header("user-agent", "").get(ClientResponse.class);
 		int response = 0;
-		if (resp.getStatus() == 200) {
-		    
-			String output = resp.getEntity(String.class);
-			//System.out.println(output);
-			
-			JSONObject jsnobject = new JSONObject(output);
-			JSONArray results = jsnobject.getJSONArray("results");
-			if(results.length()==1) {
-				JSONObject first = results.getJSONObject(0);
-				JSONArray films = first.getJSONArray("films");
-				// System.out.println(films.length());
-				response = films.length();
+
+		try {
+
+			WebResource wr = c.resource("https://swapi.co/api/planets/?search=" + name);
+			ClientResponse resp = wr.accept("application/json").header("user-agent", "").get(ClientResponse.class);
+
+			if (resp.getStatus() == 200) {
+
+				String output = resp.getEntity(String.class);
+				// System.out.println(output);
+
+				JSONObject jsnobject = new JSONObject(output);
+				JSONArray results = jsnobject.getJSONArray("results");
+				if (results.length() == 1) {
+					JSONObject first = results.getJSONObject(0);
+					JSONArray films = first.getJSONArray("films");
+					// System.out.println(films.length());
+					response = films.length();
+				}
 			}
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
-		
+
 		return response;
 	}
 
